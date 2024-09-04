ac9de158ff60a5680fe732873a9162d65832d64d
Skyeeer <charlie03.pl@gmail.com>
2024-09-04 17:42:51 +0200

    Lagt till två nya routes. Måste testas med riktig data

--- routes/routes.js
+++ routes/routes.js
@@ -25,6 +25,55 @@ router.get("/products/total-value", async (req, res) => {
     }
 });
 
+router.get("/products/low-stock", async (req, res) => {
+    try {
+        const lowStockProducts = await Product.find({ amountInStock: { $lt: 10 } });
+        if (lowStockProducts.length === 0) {
+            return res.status(200).json({
+                message: "No products are low in stock",
+                products: []
+            });
+        }
+        res.status(200).json(lowStockProducts);
+    } catch (err) {
+        res.status(500).json({ message: "Error fetching low stock products" });
+    }
+
+});
+
+router.get("/products/total-value-by-manufacturer", async (req, res) => {
+    try {
+        const result = await Product.aggregate([
+            {
+                $group: {
+                    _id: "$manufacturer",
+                    totalValue: {
+                        $sum: { $multiply: ["$price", "$amountInStock"] }
+                    }
+                }
+            }, {
+                $lookup: {
+                    from: "manufacturers",
+                    localField: "_id",
+                    foreignField: "_id",
+                    as: "manufacturerDetails"
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    manufacturer: "$manufacturerDetails.name",
+                    totalValue: 1
+                }
+            }
+        ]);
+        res.status(200).json(result);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Error calculating total value of products by manufacturer" });
+    }
+})
+
 
 
 module.exports = router;

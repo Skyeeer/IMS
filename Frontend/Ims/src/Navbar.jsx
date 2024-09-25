228d2e9b6d41963c6a61dc874fee9425684fb7e2
Skyeeer <charlie03.pl@gmail.com>
2024-09-25 12:36:16 +0200

    Css stil

--- Frontend/Ims/src/Navbar.jsx
+++ Frontend/Ims/src/Navbar.jsx
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import styles from "./styles/Navbar.module.css"
 
 export default function Navbar() {
@@ -6,10 +6,10 @@ export default function Navbar() {
     return (
         <nav className={styles.navbar}>
             <ul className={styles.list}>
-            <Link to="/"><li className={styles["menu-item"]}>Home</li></Link>
-            <Link to="/products"><li className={styles["menu-item"]}>Shop</li></Link>
-            <Link to="/manufacturers"><li className={styles["menu-item"]}>Brands</li></Link>
-            <Link to="/createproduct"><li className={styles["menu-item"]}>New Product</li></Link>
+                <Link to="/"><li className={styles["menu-item"]}>Home</li></Link>
+                <Link to="/products"><li className={styles["menu-item"]}>Shop</li></Link>
+                <Link to="/manufacturers"><li className={styles["menu-item"]}>Brands</li></Link>
+                <Link to="/createproduct"><li className={styles["menu-item"]}>New Product</li></Link>
             </ul>
         </nav>
     )

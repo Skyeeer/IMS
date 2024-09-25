57cabefdaf41998151618c07c18678d46d3be7f6
Jakob-mbi <jakob_mbi@msn.com>
2024-09-25 09:54:41 +0200

    fix type price as string

--- Frontend/Ims/src/mutations/productMutation.js
+++ Frontend/Ims/src/mutations/productMutation.js
@@ -4,7 +4,7 @@ export const CREATE_PRODUCT = gql`
     mutation createProduct(
     $name: String!, 
     $sku: String!,
-    $price: Float!,
+    $price: String!,
     $description: String!,
     $category: String!,
     $amountInStock: Int!,
